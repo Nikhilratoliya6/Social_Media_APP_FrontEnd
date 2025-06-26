61040e9c2dacd8ee728017bc5bad0f30348c7724
Nikhil Ratoliya <nikhilratoliya9@gmail.com>
2025-06-26 12:09:09 +0530

    ui4

--- src/components/Message/UserChatCard.jsx
+++ src/components/Message/UserChatCard.jsx
@@ -1,12 +1,21 @@
 import { Avatar, CardHeader, IconButton } from "@mui/material";
 import { red } from "@mui/material/colors";
 import React from "react";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { useSelector } from "react-redux";
+import PropTypes from 'prop-types';
 
 const UserChatCard = ({ item }) => {
   const { auth } = useSelector((store) => store);
+
+  if (!item) {
+    return null; // Or return a fallback UI component
+  }
+
+  const lastMessage = item.messages && item.messages.length > 0 
+    ? item.messages[item.messages.length - 1]?.content 
+    : '';
+
   return (
     <CardHeader
       avatar={
@@ -15,25 +24,37 @@ const UserChatCard = ({ item }) => {
             width: "3.5rem",
             height: "3.5rem",
             fontSize: "1.5rem",
-            bgcolor:"#191c29",color:"rgb(88,199,250)"
+            bgcolor: "#191c29",
+            color: "rgb(88,199,250)"
           }}
-          aria-label="recipe"
-          src={item.image}
-        />
-         
-       
+          aria-label="user avatar"
+          src={item.image || undefined}
+          alt={item.chat_name || 'User'}
+        >
+          {!item.image && (item.chat_name?.[0]?.toUpperCase() || '?')}
+        </Avatar>
       }
       action={
         <IconButton aria-label="settings">
           <MoreHorizIcon />
         </IconButton>
       }
-      title={
-        item.chat_name
-      }
-      subheader={item.messages[item.messages.length - 1]?.content}
+      title={item.chat_name || 'Unknown User'}
+      subheader={lastMessage || 'No messages yet'}
     />
   );
+};
+
+UserChatCard.propTypes = {
+  item: PropTypes.shape({
+    image: PropTypes.string,
+    chat_name: PropTypes.string,
+    messages: PropTypes.arrayOf(
+      PropTypes.shape({
+        content: PropTypes.string
+      })
+    )
+  })
 };
 
 export default UserChatCard;
